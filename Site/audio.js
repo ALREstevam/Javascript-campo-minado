259ee62724850a1a6d08dd58611f020cc2956229
ALREstevam <a166348@g.unicamp.com>
2017-10-31 22:40:55 -0200

    alt

---
+++ Site/audio.js
@@ -0,0 +1,19 @@
+/**
+ * Created by andre on 30/10/2017.
+ */
+
+var files =
+{
+    loose: 'bomb-contdown.mp3',
+    win: 'win.mp3',
+    start: 'game_start.mp3',
+    click: 'click_0.mp3',
+    click2: 'click_1.mp3',
+    loose2: 'explosion_n_song.mp3'
+};
+
+function playSound(filename)
+{
+    var audio = new Audio(filename);
+    audio.play();
+}
